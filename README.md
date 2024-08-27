f51af6f6ae2e8e1a77eaa06f5ad7a5339bde8cdb
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-08-27 17:08:40 +0530

    -

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tiny-crawler
+# Tiny website crawler in Python
+Crawls a website completely using breadth-first search
+This is a respectful crawller only does 1 request at a time
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import sqlite3
+con = sqlite3.connect("crawled.db")
+cur = con.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS websites(
+        netloc TEXT,
+        endpoint TEXT,
+        size INTEGER,
+        webpage_content TEXT
+    )
+""")
+
 BASE_URL = "https://pythonprogramming.net"
 import requests
 from urllib.parse import urlparse, urlunparse,urljoin
@@ -9,21 +22,44 @@ class Crawler():
 		self.base_url = base_url
 		self.parsed_base_url = urlparse(base_url)
 		self.netloc = self.parsed_base_url.netloc		
-		self.links = set()
 		self.parsed_links = set()
+		self.parsed_links.add(self.parsed_base_url)
+		self.crawled_links = set()
 
 	def crawl(self):
-		soup = self.get_soup(self.base_url)
+		while (len(self.parsed_links) != 0):
+			if len(self.parsed_links) == 0:
+				return 
+			link = self.parsed_links.pop()
+			print(f"Crawling {link.geturl()} -{len(self.crawled_links)}/{len(self.parsed_links)}")
+			html_doc = self.get_html_doc(link.geturl())
+			cur.execute("""INSERT INTO websites (netloc, endpoint, size, webpage_content) VALUES (?, ?, ?, ?)
+			""", (link.netloc, link.path, len(html_doc), html_doc))
+			con.commit()
 
-		for tag in soup.find_all("a"):
-			href = tag.get("href")
-			href = self.sanitize_href(href, self.base_url).strip().rstrip('/') # Removed trailing /
-			parsed_href = urlparse(href)._replace(fragment='')
-			if (parsed_href.netloc == self.parsed_base_url.netloc):
-				self.links.add(parsed_href.geturl())
+
+			soup = self.get_soup(html_doc)
+			for tag in soup.find_all("a"):
+				href = tag.get("href")
+				href = self.sanitize_href(href, self.base_url).strip().rstrip('/') 
+				parsed_link = urlparse(href)._replace(fragment='')
+				if self.same_base_url(parsed_link):
+					if not parsed_link in self.crawled_links:
+						self.parsed_links.add(parsed_link)
+			self.crawled_links.add(link)
+
+	def same_base_url(self, link):
+		if (link.netloc == self.parsed_base_url.netloc):
+			return True
+
 	@staticmethod
-	def get_soup(website_url):
-		html_doc = requests.get(website_url).text.strip()
+	def get_html_doc(link):
+		html_doc = requests.get(link).text.strip()
+		return html_doc 
+
+
+	@staticmethod
+	def get_soup(html_doc):
 		soup = BeautifulSoup(html_doc, "html.parser")
 		return soup
 
@@ -44,6 +80,6 @@ class Crawler():
 
 
 v_bot = Crawler(BASE_URL)
-v_bot.crawl()
-for link in v_bot.links:
-	print(link)
+print(v_bot.parsed_links)
+v_bot.crawl() 
+print(v_bot.parsed_links)
